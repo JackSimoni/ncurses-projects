b26b3a25982a77620e38c432564992761d05edf9
Jack Simoni <142266154+JackSimoni@users.noreply.github.com>
2023-08-23 17:41:53 -0400

    Update nfunctions.h

--- nfunctions.h
+++ nfunctions.h
@@ -18,7 +18,7 @@ void destroy_win(WINDOW *localwin)
 void wprint2DArray(WINDOW *localwin, int height, int width, int starty, int startx, char character)
 {
 
- for(int i = starty; i <= height; i++)
+  for(int i = starty; i <= height; i++)
          for(int j = startx; j <= width; j++)
                  mvwaddch(localwin, i, j, character);
 }
